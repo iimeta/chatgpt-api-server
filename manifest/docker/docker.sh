3a9b2f69edc303336e62265b4bb89c983a0839a4
Li Dong <cnlidong@live.cn>
2023-05-27 16:50:15 +0800

    调整构建发布脚本

--- manifest/docker/docker.sh
+++ manifest/docker/docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # This shell is executed before docker build.
+# 显示当前目录
+cd frontend && yarn build && cd ..
 
 
 
---
+++ releash.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+
+gf docker main.go -p -t xyhelper/chatgpt-api-server:latest
