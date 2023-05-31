b202378b008f0f34985a011db90d0bab04c5f569
Li Dong <cnlidong@live.cn>
2023-05-31 20:50:46 +0800

    调整流式回应支持正常中断请求

--- backend-api/conversation.go
+++ backend-api/conversation.go
@@ -3,7 +3,9 @@ package backendapi
 import (
 	"chatgpt-api-server/config"
 	"chatgpt-api-server/modules/chatgpt/model"
+	"fmt"
 	"io"
+	"net/http"
 	"time"
 
 	"github.com/cool-team-official/cool-admin-go/cool"
@@ -52,7 +54,7 @@ func Conversation(r *ghttp.Request) {
 		r.Response.WriteStatusExit(500)
 	}
 	// 如果配置了  USERTOKENLOCK ,则加锁限制每个用户只能有一个会话并发
-	if config.USERTOKENLOCK(ctx) {
+	if config.USERTOKENLOCK(ctx) && isPlusModel {
 		g.Log().Debug(ctx, "USERTOKENLOCK", config.USERTOKENLOCK(ctx))
 		// 如果 USERTOKENLOCKMAP 中没有这个用户的锁，则创建一个
 		if _, ok := USERTOKENLOCKMAP[userToken]; !ok {
@@ -60,11 +62,13 @@ func Conversation(r *ghttp.Request) {
 		}
 		// 加锁
 		if USERTOKENLOCKMAP[userToken].TryLock() {
+			g.Log().Debug(ctx, userToken, "加锁USERTOKENLOCK")
 			// 延迟解锁
 			defer func() {
 				// 延时1秒
 				time.Sleep(time.Second)
 				USERTOKENLOCKMAP[userToken].Unlock()
+				g.Log().Debug(ctx, userToken, "解锁USERTOKENLOCK")
 			}()
 		} else {
 			g.Log().Info(ctx, userToken, "触发USERTOKENLOCK,返回429")
@@ -75,11 +79,13 @@ func Conversation(r *ghttp.Request) {
 
 	// 加锁 防止并发
 	sessionPair.Lock.Lock()
+	g.Log().Debug(ctx, userToken, "加锁sessionPair.Lock")
 	// 延迟解锁
 	defer func() {
 		// 延时1秒
 		time.Sleep(time.Second)
 		sessionPair.Lock.Unlock()
+		g.Log().Debug(ctx, userToken, "解锁sessionPair.Lock")
 	}()
 	client := g.Client()
 	client.SetHeader("Authorization", sessionPair.AccessToken)
@@ -92,6 +98,17 @@ func Conversation(r *ghttp.Request) {
 	}
 	defer resp.Close()
 	if resp.StatusCode == 200 && resp.Header.Get("Content-Type") == "text/event-stream; charset=utf-8" {
+		r.Response.Header().Set("Content-Type", "text/event-stream")
+		r.Response.Header().Set("Cache-Control", "no-cache")
+		r.Response.Header().Set("Connection", "keep-alive")
+		//  流式回应
+		rw := r.Response.RawWriter()
+		flusher, ok := rw.(http.Flusher)
+		if !ok {
+			g.Log().Error(ctx, "rw.(http.Flusher) error")
+			r.Response.WriteStatusExit(500)
+			return
+		}
 		conversationId := ""
 		modelSlug := ""
 		decoder := eventsource.NewDecoder(resp.Response.Body)
@@ -123,8 +140,15 @@ func Conversation(r *ghttp.Request) {
 			if model_slug != "" {
 				modelSlug = model_slug
 			}
-			r.Response.Writefln("data: %s\n\n", text)
-			r.Response.Flush()
+			// r.Response.Writefln("data: %s\n\n", text)
+			// r.Response.Flush()
+			_, err = fmt.Fprintf(rw, "data: %s\n\n", text)
+
+			if err != nil {
+				g.Log().Error(ctx, "fmt.Fprintf error", err)
+				break
+			}
+			flusher.Flush()
 		}
 		g.Log().Debug(ctx, "conversationId", conversationId)
 		g.Log().Debug(ctx, "modelSlug", modelSlug)
@@ -136,6 +160,7 @@ func Conversation(r *ghttp.Request) {
 				"conversationId": conversationId,
 			})
 		}
+		r.ExitAll()
 
 		return
 
