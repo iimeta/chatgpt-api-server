1724ca8a5777d9e4a79e16057ea1f32982f9e240
Li Dong <cnlidong@live.cn>
2023-06-05 20:17:59 +0800

    版本记录增加commit id

--- releash.sh
+++ releash.sh
@@ -6,7 +6,9 @@ set -e
 gf docker main.go -t xyhelper/chatgpt-api-server:latest
 # 修改镜像标签为当前日期时间
 time=$(date "+%Y%m%d%H%M%S")
-docker tag xyhelper/chatgpt-api-server:latest xyhelper/chatgpt-api-server:$time
+# 获取当前git commit id 
+commit_id=$(git rev-parse HEAD)
+docker tag xyhelper/chatgpt-api-server:latest xyhelper/chatgpt-api-server:$time-$commit_id
 # 推送镜像到docker hub
 docker push xyhelper/chatgpt-api-server:latest
-docker push xyhelper/chatgpt-api-server:$time
+docker push xyhelper/chatgpt-api-server:$time-$commit_id
