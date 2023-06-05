dd67cb22f02af8da17a7769ea6313eacc8f8adfd
Li Dong <cnlidong@live.cn>
2023-06-05 20:15:53 +0800

    增加API接口

--- modules/chatgpt/controller/controller.go
+++ modules/chatgpt/controller/controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	_ "chatgpt-api-server/modules/chatgpt/controller/admin"
+	_ "chatgpt-api-server/modules/chatgpt/controller/adminapi"
 	_ "chatgpt-api-server/modules/chatgpt/controller/app"
 	_ "chatgpt-api-server/modules/chatgpt/service"
 )
