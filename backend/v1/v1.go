fa9eedd170e3f0dbe0fa73e3c7ad211797bb98fe
Gcslaoli <cnlidong@live.cn>
2024-03-03 11:50:01 +0800

    20240303114832 修正跨域

--- backend/v1/v1.go
+++ backend/v1/v1.go
@@ -4,12 +4,20 @@ import (
 	"chatgpt-api-server/v1/chat"
 
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/net/ghttp"
 )
 
 func init() {
 	s := g.Server()
 	v1Group := s.Group("/v1")
-	v1Group.ALL("/chat/completions", chat.Completions)
-	v1Group.ALL("/chat/gpt4v", chat.Gpt4v)
-	v1Group.ALL("/chat/gpt4v-mobile", chat.Gpt4v)
+	v1Group.Middleware(MiddlewareCORS)
+	v1Group.POST("/chat/completions", chat.Completions)
+	v1Group.POST("/chat/gpt4v", chat.Gpt4v)
+	v1Group.POST("/chat/gpt4v-mobile", chat.Gpt4v)
+
+}
+
+func MiddlewareCORS(r *ghttp.Request) {
+	r.Response.CORSDefault()
+	r.Middleware.Next()
 }
